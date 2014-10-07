afbc851b26d484f4be5a74a7abdc91967ce8912b
Evgeny.Svirin <jogikint@gmail.com>
2014-10-08 01:53:03 +0400

    Now slicing correct!

--- BCIdat_slicer.r
+++ BCIdat_slicer.r
@@ -11,13 +11,15 @@ for (i in (1:dim(data)[1]))
 	if (data[i, 15] != 0)
 		marks <- append(marks, c(i))
 }
-#return(marks)
-for (m in 1:length(marks))
-{
+# return(marks)
+for (m in (1:length(marks)))
+  {
   if ((data[marks[m], 15] == 2) & (data[marks[m+1], 15] != 2))
-    return(marks[m])
+      {
+      datares = data[(marks[m]+1):dim(data)[1], ]
+      break
+      }
+  }
+return(datares)
 }
 
-data <- data[29219:dim(data)[1], ]
-return(data)
-}
